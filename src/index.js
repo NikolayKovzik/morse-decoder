dc127b7c5f906d91cbce0e4994eafb6586dd1d2a
Nikolay <nikolay.kovzik@gmail.com>
2022-01-28 03:03:28 +0300

    feat: add morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let morseToLettersMap = new Map(Object.entries(MORSE_TABLE));
+    morseToLettersMap.set(' ', ' ');
+    let digitsToMorseMap = new Map();
+    digitsToMorseMap.set('10','.').set('11','-').set('00','');
+    let parseExpr = [];
+    let interm = '';
+    let result = [];
+
+    for(let i = 0; i< expr.length; i+=10){
+        parseExpr.push(expr.substr(i,10));
+    }
+    
+    morseString = parseExpr.map(function(item){
+        if(item === '**********') 
+            return ' ';
+        interm = '';
+        for(let i = 0; i < 10; i+=2)
+            interm += digitsToMorseMap.get(item[i]+item[i+1])
+        return interm;
+    });
+
+    result = morseString.map(function(item){
+        return morseToLettersMap.get(item);
+    });
+    return result.join('');
 }
 
 module.exports = {
